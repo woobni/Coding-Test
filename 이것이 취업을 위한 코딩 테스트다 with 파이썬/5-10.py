7a6172a1690472597c430ef81462932734d6156f
woobni <woobin0357@gmail.com>
2022-03-16 18:27:47 +0900

    coding test

---
+++ 이것이 취업을 위한 코딩 테스트다 with 파이썬/5-10.py
@@ -0,0 +1,40 @@
+# key point
+# 0인 값에서 상하좌우로 연결되어 있는 노드를 묶기
+# DFS
+
+# n, m을 공백으로 구분하여 입력받기
+n, m = map(int, input().split())
+
+# 2차원 리스트의 맵 정보 입력받기
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+# DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
+def dfs(x: int, y: int) -> bool:
+    # 주어진 범위를 벗어나는 경우에는 즉시 종료
+    if x <= -1 or x >= n or y <= -1 or y >= m: # x -> 세로 길이, y -> 가로 길이
+        return False
+
+    # 현재 노드를 아직 방문하지 않았다면
+    if graph[x][y] == 0:
+        # 해당 노드 방문 처리
+        graph[x][y] = 1
+        # 상하좌우의 위치도 모두 재귀 호출
+        dfs(x-1, y)
+        dfs(x, y-1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        
+        return True
+    return False
+
+# 모든 노드(위치)에 대해 묶음 찾기
+result = 0
+for i in range(n):
+    for j in range(m):
+        # 현재 위치에서 DFS 수행
+        if dfs(i, j) == True:
+            result += 1
+
+print(result)
