549183922b0bb1a1f434f2346392a1df726a780c
woobni <woobin0357@gmail.com>
2022-03-30 15:00:24 +0900

    coding test

--- 이것이 취업을 위한 코딩 테스트다 with 파이썬/7-8.py
+++ 이것이 취업을 위한 코딩 테스트다 with 파이썬/7-8.py
@@ -2,20 +2,19 @@
 
 # 잘랐을 때의 떡의 양 계산하는 함수
 def cutting(arr, h, n):
-    total =0
+    total = 0
     for i in range(n):
         if arr[i] > h:
             total += arr[i] - h
     return total
 
-# def cutting(arr, h, n):
-#     for i in range(n):
+# def cutting(arr, h):
+#     for i in range(len(arr)):
 #         temp = arr[i]- h
 #         if temp < 0:
 #             arr[i] = 0
 #         else:
 #             arr[i] = temp
-
 #     return sum(arr)
 
 # 떡의 개수 N, 요청한 떡의 길이 M 입력받기
@@ -29,12 +28,12 @@ h_list = []
 while start <= end :
     h = ( start + end ) // 2
 
-    # 찾고자 하는 값이 작은 경우 왼쪽 확인
-    if cutting(arr, h, n) < m :
+    # 요청한 떡의 길이보다 작은 경우 왼쪽 확인
+    if cutting(arr, h) < m :
         end = h - 1
 
-    # 찾고자 하는 값이 큰 경우 오른쪽 확인
-    else : # cutting(arr, h, n) >= m
+    # 요청한 떡의 길이보다 충분한 경우 오른쪽 확인
+    else : # cutting(arr, h) >= m
         h_list.append(h) # 최소한 m이 나오면 되고 최대의 h를 구해야 하므로
         start = h + 1
 
