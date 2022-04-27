c5456ca98f2ada642d557c7b1c539412f231ab32
woobni <woobin0357@gmail.com>
2022-04-27 14:01:45 +0900

    feat: coding test

---
+++ baekjoon/parrot.py
@@ -0,0 +1,48 @@
+from typing import List
+from collections import deque
+
+# 앵무새의 수 n 입력받기
+n = int(input())
+
+arr = []
+# 앵무새가 말한 문장 입력받아서 큐로 저장
+for _ in range(n):
+    arr.append(deque(input().split())) 
+
+def possible(sentence: List[str], arr: List[deque]) -> bool :
+    i = 0
+    cnt = 0
+    # sentence가 빌 때까지 반복해서 실제 문장의 단어가 있는지 확인
+    while sentence:
+        if arr[i] and sentence[0] == arr[i][0]:
+            arr[i].popleft()
+            sentence.popleft()
+            cnt = 0
+
+        else: 
+            if cnt == n: # 실제 문장의 단어가 세 앵무새의 큐에 없을 때
+                return False
+            cnt += 1
+        
+        i = (i + 1) % n # i = 0, 1, 2
+
+    # while 문이 끝났다는 건 sentence가 다 pop돼서 비었다는 뜻
+    # 앵무새가 모든 단어들을 다 말해야 하므로 각 앵무새의 큐가 비어있는지 확인
+    empty = 0
+    for j in range(n):
+        if not arr[j]:
+            empty += 1
+    
+    if empty == n:
+        return True
+    else:
+        return False
+
+
+# 실제 문장 입력받기
+sentence = deque(input().split())
+
+if possible(sentence, arr):
+    print("Possible")
+else:
+    print("Impossible")
